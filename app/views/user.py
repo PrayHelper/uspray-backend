0b05f97ed66df47889cf9d7df2f44f3f824fcb4c
subin <200516bb@gmail.com>
2023-03-24 13:15:08 +0900

    feat: login api

--- app/views/user.py
+++ app/views/user.py
@@ -4,6 +4,7 @@ from flask import request, g, jsonify
 import bcrypt
 import datetime
 import requests
+import jwt
 
 from app import db
 from app.models.user import User
@@ -18,6 +19,11 @@ userModel = user.model('User', {
     'gender': fields.String(required=True, description='user gender'),
     'birth': fields.Date(required=True, description='user birth'),
     'phone': fields.String(required=True, description='user phone'),
+})
+
+loginModel = user.model('Login', {
+    'uid': fields.String(required=True, decription='user id'),
+    'password': fields.String(required=True, decription='user password')
 })
 
 @user.route('/signup', methods=['POST'])
@@ -36,8 +42,8 @@ class SignUp(Resource):
             birth=content['birth'],
             phone=content['phone']
         )
-        print(u)
         return {'message': '회원가입에 성공하였습니다.'}
+
 
 @user.route('/dup_check', methods=['GET'])
 class IdDupCheck(Resource):
@@ -51,3 +57,28 @@ class IdDupCheck(Resource):
         if dupUserId is None:
             return jsonify({'dup' : False})
         return jsonify({'dup' : True})
+
+
+@user.route('/login', methods=['POST'])
+class Login(Resource):
+    @user.expect(loginModel)
+    def post(self):
+        """
+        Login
+        """
+        content = request.json
+
+        u = User.query.filter_by(uid=content['uid']).first
+
+        if u is None:
+            return { 'message' : '아이디가 존재하지 않습니다.' }
+        
+        if bcrypt.checkpw(content['password'].encode('UTF-8'), u.password):
+            payload = {
+                'uid': u.uid,
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60 * 24)
+            }
+            token = jwt.encode(payload, "secret", algorithm="HS256")
+            return { 'message' : '로그인에 성공하였습니다.', 'access_token': token }, 200
+        else:
+            return { 'message' : '비밀번호를 잘못 입력하였습니다.' }
