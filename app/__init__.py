4700a7f98fe2709ec055bb848238d3cf75f6ae39
subin <200516bb@gmail.com>
2023-03-27 15:06:39 +0900

    pray list test api (오류)

--- app/__init__.py
+++ app/__init__.py
@@ -29,13 +29,15 @@ def create_app():
         security='basic'
     )
     from .views.user import user
+    from .views.pray import pray
     api.add_namespace(main, '')
     api.add_namespace(user, '/user')
+    api.add_namespace(pray, '/pray')
     app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
 
     # ORM
     from .models import user
-    from .models import prayrequest
+    #from .models import pray
     db.init_app(app)
     migrate.init_app(app, db)
 
