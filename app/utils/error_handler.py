9b1a170a2ebd861fa55d4fe4c6183dc276ed1f13
baebae02 <bae4614@gmail.com>
2023-05-02 11:11:35 +0900

    feat: add refresh_token

--- app/utils/error_handler.py
+++ app/utils/error_handler.py
@@ -44,6 +44,17 @@ class StorageFail(CustomUserError):
         
     def to_dict(self):
         return self.rv
+    
+
+class InvalidTokenError(CustomUserError):
+    def __init__(self, error_message, dev_error_message=None):
+        status_code = 400
+        if not dev_error_message:
+            dev_error_message = "LoginFail error"
+        super().__init__(status_code, dev_error_message, error_message)
+        
+    def to_dict(self):
+        return self.rv
 
 def handle_custom_user_error(error):
     response = jsonify(error.to_dict())
