0f303b4c59f694cf9fbb4c6a8428d8f0a5aee0b5
subin <200516bb@gmail.com>
2023-08-07 20:23:45 +0900

    error resolve

--- app/api/user.py
+++ app/api/user.py
@@ -355,7 +355,7 @@ class KakaoOauth(Resource):
                 "code": code,
             }, 
         ).json()
-        return (oauth_token)
+
         kakao_access_token = oauth_token["access_token"]
         profile_request = requests.get(
             "https://kapi.kakao.com/v2/user/me", headers={"Authorization": f"Bearer {kakao_access_token}"}
